59a0f408420966cbdf629d2414c42d6e301d3e64
Neil Gebhard <neilgebhard@gmail.com>
2022-01-13 18:43:10 +1000

    :white_check_mark: Add small update to tests

--- frontend/src/__tests__/Feed.test.tsx
+++ frontend/src/__tests__/Feed.test.tsx
@@ -6,7 +6,6 @@ describe("Feed", () => {
     render(<Feed />);
     expect(screen.getByRole("heading", { name: /home/i })).toBeInTheDocument();
     expect((await screen.findAllByRole("listitem")).length).toBe(1);
-
     expect(
       screen.getByText(
         /It's estimated that 240,000 new homes will have to be built every year in the UK for the next 20 years to satisfy the need for housing/i
